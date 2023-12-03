264777d1ce60df26e52f3e397cedac2c5f532338
Darshit Desai <36150235+darshit-desai@users.noreply.github.com>
2023-12-03 15:10:20 -0500

    Added recent changes to this branch. !!!!!!!!!!!!!!!!!!!NO MORE COMMITS!!!!!!!!!!!!!!!!!!!! without a Google Meet

---
+++ models/hyponets/hypo_shacira.py
@@ -0,0 +1,149 @@
+import os
+import argparse
+import logging
+import numpy as np
+import wandb
+import torch
+import wisp
+import json
+from wisp.app_utils import default_log_setup, args_to_log_format, copy_dir_msrsync, copy_dir, copy_file
+import wisp.config_parser as config_parser
+from wisp.framework import WispState, ImageState
+from wisp.models.grids import BLASGrid, LatentGrid
+from wisp.models.latent_decoders.basic_latent_decoder import DecoderIdentity
+from wisp.models.latent_decoders.multi_latent_decoder import MultiLatentDecoder
+from wisp.tracers import BaseTracer, PackedRFTracer
+from wisp.models.nefs import BaseNeuralField, NeuralImage
+from wisp.models.pipeline import Pipeline
+from models import register
+
+
+def load_grid() -> BLASGrid:
+    """ Wisp's implementation of NeRF uses feature grids to improve the performance and quality (allowing therefore,
+    interactivity).
+    This function loads the feature grid to use within the neural pipeline.
+    Grid choices are interesting to explore, so we leave the exact backbone type configurable,
+    and show how grid instances may be explicitly constructed.
+    Grids choices, for example, are: OctreeGrid, TriplanarGrid, HashGrid,LatentGrid, CodebookOctreeGrid
+    See corresponding grid constructors for each of their arg details.
+    """
+    grid = None
+    # Optimization: For octrees based grids, if dataset contains depth info, initialize only cells known to be occupied
+    # WARNING: Not implemented/tested for grids other than HashGrid/LatentGrid 
+    conf_latent_decoder = {'ldecode_enabled': True, 'ldecode_type': 'single', 'use_sga': True, 'diff_sampling': True, 'ldecode_matrix': 'sq', 'latent_dim': 1, 'norm': 'max', 'norm_every': 10, 'use_shift': True, 'num_layers_dec': 0, 'hidden_dim_dec': 0, 'activation': 'none', 'final_activation': 'none', 'clamp_weights': 0.0, 'ldec_std': 0.1, 'num_decoders': 2, 'temperature': 0.1, 'decay_period': 0.9, 'alpha_std': 10.0}
+    conf_entropy_reg = {'num_prob_layers': 2, 'entropy_reg': 0.001, 'entropy_reg_end': 0.0001, 'entropy_reg_sched': 'cosine', 'noise_freq': 1}
+    # "geometric" - determines the resolution of the grid using geometric sequence initialization from InstantNGP,
+
+    grid = LatentGrid.from_geometric(
+        feature_dim=1,
+        latent_dim=conf_latent_decoder["latent_dim"],
+        num_lods=16,
+        multiscale_type='cat',
+        resolution_dim=2,
+        feature_std=0.1,
+        feature_bias=0.0,
+        codebook_bitwidth=12,
+        min_grid_res=16,
+        max_grid_res=512,
+        blas_level=7,
+        init_grid='uniform',
+        conf_latent_decoder=conf_latent_decoder,
+        conf_entropy_reg=conf_entropy_reg
+        )
+    return grid
+
+
+def load_neural_field() -> NeuralImage:
+    """ Creates a "Neural Field" instance which converts input coordinates to some output signal.
+    Here a NeuralRadianceField is created, which maps 2D coordinates -> RGB.
+    The NeuralRadianceField uses spatial feature grids internally for faster feature interpolation and raymarching.
+    """
+    grid = load_grid()
+    nef = NeuralImage(
+        grid=grid,
+        pos_embedder='none',
+        position_input=False,
+        pos_multires=10,
+        activation_type='relu',
+        layer_type='none',
+        hidden_dim=16,
+        num_layers=1,
+    )
+    return nef
+
+def load_neural_pipeline(device) -> Pipeline:
+    """ In Wisp, a Pipeline comprises of a neural field + a tracer (the latter is optional in this image case).
+    Together, they form the complete pipeline required to render a neural primitive from input rays / coordinates.
+    """
+    nef = load_neural_field()
+    pipeline = Pipeline(nef=nef, tracer=None)
+    # if args.pretrained:
+    #     if args.model_format == "full":
+    #         pipeline = torch.load(args.pretrained)
+    #     else:
+    #         pipeline.load_state_dict(torch.load(args.pretrained))
+    pipeline.to(device)
+    return pipeline
+
+#Write a class from the above functions note that the forward function is already there in the pipeline.py of the wisp library folder
+@register('hypo_shacira')
+class HypoShacira(torch.nn.Module):
+    def __init__(self):
+        super(HypoShacira, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.pipeline = load_neural_pipeline(self.device)
+        self.param_shapes = dict()
+        i = 0
+        # self.layers_dict = dict()
+        for (name, param) in self.pipeline.named_parameters():
+            # if param.shape == torch.Size([1, 1]) and (name != "nef.grid.latent_dec.layers.0.scale" or name != "nef.grid.latent_dec.layers.0.shift"):
+            #     continue
+            if param.shape == torch.Size([47737, 1]):
+                self.param_shapes[f'wb{i}'] = param.shape
+                # self.layers_dict[f'wb{i}'] = [name, param, param.shape]
+                print(f'wb{i}', name, param.shape)
+            elif param.shape == torch.Size([16, 16]):
+                # shape = param.shape
+                self.param_shapes[f'wb{i}'] = param.shape
+                # self.layers_dict[f'wb{i}'] = [name, param, param.shape]
+                print(f'wb{i}', name, param.shape)
+            elif param.shape == torch.Size([3,16]):
+                # shape = param.shape
+                self.param_shapes[f'wb{i}'] = param.shape
+                # self.layers_dict[f'wb{i}'] = [name, param, param.shape]
+                print(f'wb{i}', name, param.shape)
+            # if i == 0:
+                # print("Inside the constructor hyposhacira: ", param.data)
+            i+=1
+        self.params = None
+    def set_params(self, params):
+        self.params = params
+    def get_codebook(self):
+        return self.pipeline.nef.grid.codebook
+    # def get_layers_dict(self):
+    #     return self.layers_dict
+    def forward(self, x):
+        # print("Xshape= ", x.shape)
+        btch = x.shape[0]
+        # print(btch)
+        op = torch.empty(btch, 178**2, 3, device=self.device)
+        for j in range(btch):
+            i = 0
+            for (name, param) in self.pipeline.named_parameters():
+                # if param.shape == torch.Size([1, 1]) and ((name != "nef.grid.latent_dec.layers.0.scale") or (name != "nef.grid.latent_dec.layers.0.shift")):
+                #     continue      
+                if param.shape == torch.Size([47737, 1]) or param.shape == torch.Size([16, 16]) or param.shape == torch.Size([3,16]):           
+                    param.data = self.params[f'wb{i}'][j, ...].data
+                i+=1
+                if i == 0 and j == 0:
+                    print(param.data)
+            # print(self.params[f'wb{0}'][0, ...].data)
+            xin =  x[j,:,:].to(self.device)
+            xin  =  xin.view(-1, 2)
+            # print(xin.shape)
+            op[j, ...] = torch.stack(self.pipeline.nef(coords=xin, channels=["rgb"]), dim=0).to(self.device)
+
+        # Reshape the output tensor to (batch_size, 178, 178, 3)
+        oup = op.view(btch, 178, 178, 3)
+        # print(oup.shape)        
+        return oup             
---
+++ models/trans_inr.py
@@ -0,0 +1,150 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
+
+import models
+from models import register
+
+
+# def init_wb(shape, is_weight=True):
+#     if is_weight:
+#         # if shape == torch.Size([1, 1]):
+#         #     # Change the shape of the weight to (2, shape[1])
+#         #     weight = torch.empty(shape[1], shape[0])
+#         # else:
+#         #     weight = torch.empty(shape[1], shape[0] - 1)
+#         weight = torch.empty(shape[1], shape[0])
+#         nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
+#         return weight.t().detach()
+#     else:
+#         val = shape[0]
+#         bias = torch.empty(val, 1)
+#         nn.init.kaiming_uniform_(bias, a=math.sqrt(5))
+#         fan_in, _ = nn.init._calculate_fan_in_and_fan_out(bias)
+#         bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 1e-5
+#         nn.init.uniform_(bias, -bound, bound)
+#         return bias.detach()
+
+def init_wb(shape):
+    weight = torch.empty(shape[1], shape[0] - 1)
+    nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
+
+    bias = torch.empty(shape[1], 1)
+    fan_in, _ = nn.init._calculate_fan_in_and_fan_out(weight)
+    bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+    nn.init.uniform_(bias, -bound, bound)
+
+    return torch.cat([weight, bias], dim=1).t().detach()
+
+
+
+@register('trans_inr')
+class TransInr(nn.Module):
+
+    def __init__(self, tokenizer, hyponet, n_groups, transformer_encoder):
+        super().__init__()
+        dim = transformer_encoder['args']['dim']
+        self.tokenizer = models.make(tokenizer, args={'dim': dim})
+        self.hyponet = models.make(hyponet)
+        self.transformer_encoder = models.make(transformer_encoder)
+        
+        self.base_params = nn.ParameterDict()
+        n_wtokens = 0
+        self.wtoken_postfc = nn.ModuleDict()
+        self.wtoken_rng = dict()
+        self.user_inp = False #True: hypo_mlp; False: hypo_Shacira
+        for name, shape in self.hyponet.param_shapes.items():
+            if self.user_inp:
+                self.base_params[name] = nn.Parameter(init_wb(shape))
+            else:
+                if shape == torch.Size([47737,1]):
+                    # self.base_params[name] = nn.Parameter(self.hyponet.get_codebook())
+                    weight = torch.empty(shape[0], shape[1])
+                    self.base_params[name] = nn.Parameter(nn.init.kaiming_uniform_(weight, a=math.sqrt(5), mode='fan_in', nonlinearity='relu'))
+                elif shape == torch.Size([16,16]):
+                    weight = torch.empty(shape[0], shape[1])
+                    self.base_params[name] = nn.Parameter(nn.init.kaiming_uniform_(weight, a=math.sqrt(5), mode='fan_in', nonlinearity='relu'))
+                elif shape == torch.Size([3,16]):
+                    weight = torch.empty(shape[0], shape[1])
+                    self.base_params[name] = nn.Parameter(nn.init.kaiming_uniform_(weight, a=math.sqrt(5), mode='fan_in', nonlinearity='relu'))
+            # print("Before shape=", shape)
+            # if shape != torch.Size([47737, 1]):
+            #     if len(shape) == 1:   
+            #         self.base_params[name] = nn.Parameter(init_wb(shape, is_weight=False))
+            #     else:
+            #         self.base_params[name] = nn.Parameter(init_wb(shape, is_weight=True))
+            # # print(self.base_params[name].shape)
+            # else:
+            #     self.base_params[name] = nn.Parameter(self.hyponet.get_codebook())
+                # print(self.base_params[name])
+            # shape = self.base_params[name].shape
+            g = min(n_groups, shape[1])
+            assert shape[1] % g == 0
+            self.wtoken_postfc[name] = nn.Sequential(
+                nn.LayerNorm(dim),
+                nn.Linear(dim, shape[0]),
+            )
+            # if shape == torch.Size([1,1]):
+            #     self.wtoken_postfc[name] = nn.Sequential(
+            #         nn.LayerNorm(dim),
+            #         nn.Linear(dim, shape[0]),
+            #     )
+            # else:
+            #     self.wtoken_postfc[name] = nn.Sequential(
+            #         nn.LayerNorm(dim),
+            #         nn.Linear(dim, shape[0] - 1),
+            #     )
+            self.wtoken_rng[name] = (n_wtokens, n_wtokens + g)
+            n_wtokens += g
+        self.wtokens = nn.Parameter(torch.randn(n_wtokens, dim))
+
+    def forward(self, data):
+        dtokens = self.tokenizer(data)
+        B = dtokens.shape[0]
+        wtokens = einops.repeat(self.wtokens, 'n d -> b n d', b=B)
+        trans_out = self.transformer_encoder(torch.cat([dtokens, wtokens], dim=1))
+        trans_out = trans_out[:, -len(self.wtokens):, :]
+        
+        params = dict()
+        for name, shape in self.hyponet.param_shapes.items():
+            wb = einops.repeat(self.base_params[name], 'n m -> b n m', b=B)
+            # print("wb shape=", wb.shape)
+            if wb.shape == torch.Size([B, 47737, 1]) or wb.shape == torch.Size([B, 16, 16]) or wb.shape == torch.Size([B, 3, 16]):
+                w = wb
+                l, r = self.wtoken_rng[name]
+                x = self.wtoken_postfc[name](trans_out[:, l: r, :])
+                x = x.transpose(-1, -2) # (B, shape[0] - 1, g)
+                w = F.normalize(w * x.repeat(1, 1, w.shape[2] // x.shape[2]), dim=1)
+                wb = w
+                params[name] = wb
+            else:
+                w, b = wb[:, :-1, :], wb[:, -1:, :]
+                # print("w shape=", w.shape)
+                # print("b shape=", b.shape)
+
+                l, r = self.wtoken_rng[name]
+                # print("l=", l)
+                # print("r=", r)
+                x = self.wtoken_postfc[name](trans_out[:, l: r, :])
+                # print("x shape=", x.shape)
+                x = x.transpose(-1, -2) # (B, shape[0] - 1, g)
+                # print("x shape=", x.shape)        
+                # w = F.normalize(w * x.repeat(1, 1, w.shape[2] // x.shape[2]), dim=1)
+                w = w * x.repeat(1, 1, w.shape[2] // x.shape[2])
+                # print("w shape=", w.shape)
+                wb = torch.cat([w, b], dim=1)
+                # print("wb shape=", wb.shape)
+                if wb.shape == torch.Size([B, 3, 1]) or wb.shape == torch.Size([B, 16, 1]):
+                    if wb.shape == torch.Size([B, 3, 1]):
+                        wb = wb.reshape(B, 3)
+                    elif wb.shape == torch.Size([B, 16, 1]):
+                        wb = wb.reshape(B, 16)
+                    # print("wb shape=", wb.shape)
+                params[name] = wb
+                # print("params[name] shape=", params[name].shape)
+
+        self.hyponet.set_params(params)
+        return self.hyponet
